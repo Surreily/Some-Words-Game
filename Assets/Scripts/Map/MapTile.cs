ca56db2852edba8b4a2c81322fe0f894a008261e
Sean Reilly <surreily1@gmail.com>
2023-07-01 19:57:06 +0100

    Remove unused properties.

--- Assets/Scripts/Map/MapTile.cs
+++ Assets/Scripts/Map/MapTile.cs
@@ -1,9 +1,5 @@
 ﻿namespace Surreily.SomeWords.Scripts.Map {
     public class MapTile {
-        public MapTile NextNorth { get; set; }
-        public MapTile NextEast { get; set; }
-        public MapTile NextSouth { get; set; }
-        public MapTile NextWest { get; set; }
         public bool IsOpen { get; set; }
     }
 }
