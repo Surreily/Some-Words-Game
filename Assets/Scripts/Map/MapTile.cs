92338d3c9a01f18fdf153638f4c670517769dc71
Sean Reilly <surreily1@gmail.com>
2023-07-13 23:49:03 +0100

    Delete some unused classes.

--- Assets/Scripts/Map/MapTile.cs
+++
@@ -1,9 +0,0 @@
-﻿using Surreily.SomeWords.Scripts.Utility;
-
-namespace Surreily.SomeWords.Scripts.Map {
-    public class MapTile {
-        public int Variation { get; set; }
-        public PathTileType Position { get; set; }
-        public bool IsOpen { get; set; }
-    }
-}
